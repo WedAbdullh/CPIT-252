41d311f1a2b6c5bfd367effba083215e06ba05d3
WedAbdullh <wedabdullh2003@gmail.com>
2024-10-22 17:30:51 +0300

    let's start

---
+++ src/cpit/pkg252/CPIT252.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package cpit.pkg252;
+
+/**
+ *
+ * @author wedalotibi
+ */
+public class CPIT252 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.print("let's get started");
+    }
+    
+}
